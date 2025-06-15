a63d7f4e658df5c5c7b33acb24dfd19a1a4936c8
Joseph France <jafcxm@umsystem.edu>
2025-06-15 18:49:43 -0500

    Initial initialization of the project. I created all of the files and added the base of the project by creating the grid container and cell.

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+//main container for the grid
+const gridContainer = document.createElement('div');
+gridContainer.id = 'grid-container';
+document.body.appendChild(gridContainer);
+
+//grid cell
+const gridCell = document.createElement('div');
+gridCell.id = 'grid-cell';
+gridContainer.appendChild(gridCell);
+
